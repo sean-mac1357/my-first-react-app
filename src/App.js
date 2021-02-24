1fcaa8a23049a59baed8a4a6c1a890d31f285093
Sean R MacEachern <sean.r.maceachern@gmail.com>
2021-02-24 14:09:01 -0500

    completed exercise from class

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Hello(props) {
+  return(
+    <>
+      <h1 className="title">Hello {props.name}!</h1>
+      <h2>Subtitle</h2>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Hello name="Sean" />
       </header>
     </div>
   );
